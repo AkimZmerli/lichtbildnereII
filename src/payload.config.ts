910b54b77c3419436ccfcb4f7a92c3a763c8b0d6
Akim Zmerli <akim.zmerli@googlemail.com>
2025-10-13 19:29:35 +0200

    add database variable directly in payload config

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -45,11 +45,7 @@ export default buildConfig({
   },
   db: postgresAdapter({
     pool: {
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
-      database: process.env.POSTGRES_DB,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
+      connectionString: process.env.DATABASE_URL,
       ssl: process.env.NODE_ENV === 'production' ? {
         rejectUnauthorized: false,
       } : false,
