0cefa0c0b10df77868af34c53661503ba3fd7444
Akim Zmerli <akim.zmerli@googlemail.com>
2025-11-03 17:24:38 +0100

    adjust social book size

--- src/features/social-book/components/SocialBook.tsx
+++ src/features/social-book/components/SocialBook.tsx
@@ -40,7 +40,7 @@ export default function SocialBook() {
               alt="The Social Book"
               width={780}
               height={405}
-              className="object-contain w-[230] h-[169]"
+              className="object-contain"
             />
           </button>
         </div>
